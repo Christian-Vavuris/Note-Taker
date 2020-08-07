e4603f6867cd0b302e0eca6cf79027850d67bd15
Christian Vavuris <christianvavuris@Christians-MacBook-Pro.local>
2020-08-07 15:20:31 -0700

    got my server up and running

--- Server.js
+++ Server.js
@@ -1,9 +1,14 @@
 
-const fs = require(fs)
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
 
 
-console.log('script.js');
+// console.log('script.js');
 
 
-
+app.listen(3001, () => {
+    console.log('API server now on port 3001!')
+});
 
