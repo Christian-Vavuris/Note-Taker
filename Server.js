d7c97d73eb73e279e9de97c3369a1d2e59a25930
Christian Vavuris <christianvavuris@Christians-MacBook-Pro.local>
2020-08-07 18:06:04 -0700

    quick heoku bug fix

--- Server.js
+++ Server.js
@@ -39,7 +39,7 @@ app.get('/api/notes', (req, res) => {
 
 // })
 
-app.listen(3001, () => {
-    console.log('API server now on port 3001!')
-});
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
 
