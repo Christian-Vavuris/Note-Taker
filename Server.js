91c47fb5101d794f439eaaf4ab29c9c2376c23ad
Christian Vavuris <christianvavuris@Christians-MacBook-Pro.local>
2020-08-10 18:37:00 -0700

    debugging heroku

--- Server.js
+++ Server.js
@@ -3,8 +3,8 @@
 const { notes } = require('./db/db.json');
 const express = require('express');
 const fs = require('fs');
-const PORT = process.env.PORT || 3001;
 const path = require('path');
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static('public'));
